4e8d2f8be1a4586eae74c8c6e719077d75498ea6
JY <1776555345@qq.com>
2020-01-11 16:48:44 +0800

    代码生成

---
+++ springboot09_voice/src/main/java/com/zc/springboot09_voice/Springboot09VoiceApplication.java
@@ -0,0 +1,13 @@
+package com.zc.springboot09_voice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot09VoiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot09VoiceApplication.class, args);
+    }
+
+}
